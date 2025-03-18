d52e1132fb21cc99da4b8e98a31d08ecebab04f8
Glitch-guy0 <prajwalm9845@gmail.com>
2025-03-18 22:33:48 +0530

    feat: database connection script

--- src/db_init.py
+++ src/db_init.py
@@ -1,17 +1,32 @@
 import asyncio
+import sys
 from motor.motor_asyncio import AsyncIOMotorClient
 from beanie import init_beanie
 import env
 from models.session_model import Session
 from models.user_model import User
+from pymongo.errors import ServerSelectionTimeoutError
 
-
-async def db_init():
-    client = AsyncIOMotorClient(f"mongodb://{env.DB_USER}:{env.DB_PASS}@{env.DB_HOST}/{env.DB_NAME}")
-
-    if not client:
-        print("Error Connecting to DB, is it running?")
-        raise SystemExit(1)
+async def db_connect():
+    # client = AsyncIOMotorClient(f"mongodb://{env.DB_USER}:{env.DB_PASS}@{env.DB_HOST}/{env.DB_NAME}", connectTimeoutMS=3000, ServerSelectionTimeoutMS=3000)
+    client = AsyncIOMotorClient(host="localhost",port=27017,username="admin", password="admin",connectTimeoutMS=3000, ServerSelectionTimeoutMS=3000)
 
     # Initialize beanie with the Product document class
     db_connection = await init_beanie(database=client.db_name, document_models=[User, Session])
+
+
+async def db_init():
+    try:
+        await db_connect()
+    except ServerSelectionTimeoutError as db_err:
+        print("Error connecting to DB, is it even runnning!!", db_err._message)
+        sys.exit(1)
+    except Exception as e:
+        print("Error Occured!!\n",e)
+        sys.exit(1)
+    else:
+        # db connection here
+        print('connected to db/ running')
+
+if __name__ == "__main__":
+    asyncio.run(db_init())
