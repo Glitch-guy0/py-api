3150ca81fe2e0919ce01885698ff4f9622ea6786
Glitch-guy0 <prajwalm9845@gmail.com>
2025-03-19 21:15:33 +0530

    feat: create user

--- src/models/user_model.py
+++ src/models/user_model.py
@@ -1,26 +1,33 @@
 
-import datetime
+
 from typing import Annotated
 from beanie import Document, Indexed
 import re
-from pydantic import EmailStr
+from pydantic import EmailStr, Field
 from fastapi import HTTPException
-
+import env
+import bcrypt
+import random
 
 class User(Document):
   """
     Collection name: users
   """
-  username: str
+  username: str = Field(min_length=3, max_length=20)
   email: Annotated[EmailStr, Indexed(unique=True)]
-  password: str
-  salt: int
+  password: bytes # bcrypt returns byte data
+  # bcrypt stores salt and other metadata internally
 
   @classmethod
-  def create(self, username, email, password):
+  def create(self, username:str, email:EmailStr, password:str):
     auth.password_validator(password)
-    return self(username=username, email=email, password=password, salt=34)
+    salt: bytes = bcrypt.gensalt(random.randint(12,15))
+    hashed_password: bytes = bcrypt.hashpw(User.__encode_password(password),salt)
+    return self(username=username, email=email, password=hashed_password)
 
+  @staticmethod
+  def __encode_password(password:str)->bytes:
+    return password.encode('utf-8')
 
 class auth:
   def password_validator(password: str) -> bool:
