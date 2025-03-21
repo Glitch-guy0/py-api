1a61c1602af17b2300a757ad76e1cea78e02cfd9
Glitch-guy0 <prajwalm9845@gmail.com>
2025-03-21 19:29:22 +0530

    fix: session model

--- src/models/session_model.py
+++ src/models/session_model.py
@@ -1,16 +1,17 @@
 
 import datetime
 from typing import Annotated
-from beanie import Document, Indexed
+from beanie import Document, Indexed, PydanticObjectId
 
 class Session(Document):
   """
     Collection name: sessions
   """
   session_token: Annotated[str, Indexed(unique=True)]
-  user_id: str
+  user_id: PydanticObjectId
   created_at: Annotated[datetime.datetime, Indexed(expireAfterSeconds=60*60*24)]
 
   @classmethod
-  def create(self, session_token, user_id):
-    return self(session_token=session_token, user_id=user_id, created_at=datetime.datetime.now())
+  async def create(cls, session_token: str, user_id: PydanticObjectId):
+    new_session = cls(session_token=session_token, user_id=user_id, created_at=datetime.datetime.now())
+    await new_session.save()
