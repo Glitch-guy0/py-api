546c676977083a40386ec0335464cdcf7c0eec4e
Glitch-guy0 <prajwalm9845@gmail.com>
2025-03-24 07:14:52 +0530

    model: user model

--- src/models/user.py
+++ src/models/user.py
@@ -1,5 +1,18 @@
 # contains user database related functions
-from beanie import Document
+from beanie import Document, Indexed, PydanticObjectId
+from pydantic import Field, EmailStr
+from typing import Annotated, Optional
+
+class Update_User(Document):
+  username: Optional[str] = Field(min_length=3, max_length=20)
+  password: Optional[str]
+
+
+class Login_Info(Document):
+  id: PydanticObjectId
+  password: bytes
 
 class User(Document):
-  name: str
+  username: str = Field(min_length=3, max_length=20)
+  email: Annotated[EmailStr, Indexed(unique=True)]
+  password: bytes
