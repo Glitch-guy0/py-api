5ae9590b4090c4dfe40cecaf75978fcdf07637be
Glitch-guy0 <prajwalm9845@gmail.com>
2025-03-26 22:54:06 +0530

    refactor: signin and login as account manager

--- src/User/route.py
+++ src/User/route.py
@@ -2,13 +2,13 @@ from fastapi import APIRouter, Request, HTTPException, Response
 router = APIRouter(prefix="/user")
 from models.user import User, Login_Data, Login_Info
 from lib.auth.session_manager import Session_Manager
-from lib.auth.login_manager import Login_Manager
+from lib.auth.account_manager import Account_Manager
 
 @router.post("/")
 async def create_user(request: Request, response: Response):
   data = await request.json()
   user_data = User(**data)
-  await User.create_user(user_data)
+  Account_Manager.create_user(user_data)
   return {"message": "User Created"}
 
 
@@ -16,6 +16,6 @@ async def create_user(request: Request, response: Response):
 async def login_user(request: Request, response: Response):
   data = await request.json()
   login_data = Login_Data(**data)
-  user_data = await Login_Manager.verify_login_data(login_data)
+  user_data = await Account_Manager.verify_user_login(login_data)
   Session_Manager.create_session(user_data, response)
   return {"detail": "Login Successful"}
--- src/lib/auth/account_manager.py
+++ src/lib/auth/account_manager.py
@@ -2,9 +2,15 @@ from models.user import Login_Info, Login_Data, User
 from lib.auth.validations import Validations
 from .crypto import Crypto
 
-class Login_Manager:
+class Account_Manager:
+
   @staticmethod
-  async def verify_login_data(login_data: Login_Data)-> Login_Info:
+  async def create_user(user_data:User)-> None:
+    await User.create_user(user_data)
+
+
+  @staticmethod
+  async def verify_user_login(login_data: Login_Data)-> Login_Info:
     Validations.password_validator(login_data.password)
     user = await User.get_user_by_email(login_data.email)
     Crypto.verify_password(login_data.password, user.password)
