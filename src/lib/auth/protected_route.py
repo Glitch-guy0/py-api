9a7d04d2eb6c42403955e21ea5956c285c5e8e2d
Glitch-guy0 <prajwalm9845@gmail.com>
2025-03-26 22:48:13 +0530

    feat: decorator to define protected routes

--- src/lib/auth/protected_route.py
+++ src/lib/auth/protected_route.py
@@ -1,1 +1,11 @@
 # provides decorator for private routes
+from fastapi import Request
+from .session_manager import Session_Manager
+
+def protected_route(fastapi_request: Request):
+  def decorator(func):
+    async def wrapper(*args, **kwargs):
+        user_id = await Session_Manager.get_session_user_id(fastapi_request)
+        return await func(user_id, *args, **kwargs)
+    return wrapper
+  return decorator
