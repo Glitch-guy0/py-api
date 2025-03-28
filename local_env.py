266106d97fdb464aac9e11c4e2971cf170ec25af
Glitch-guy0 <prajwalm9845@gmail.com>
2025-03-28 21:14:36 +0530

    dev: local env

---
+++ local_env.py
@@ -0,0 +1,7 @@
+DB_NAME='py-api'
+DB_USER='admin'
+DB_PASS='admin'
+DB_HOST='localhost'
+DB_PORT=27017
+JWT_SECRET="secretCode"
+LOG_VERBOSITY="DEBUG"              # can be (DEBUG, INFO, WARNING, ERROR, CRITICAL)
