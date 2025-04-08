4226c3897579250208a2c01b6b0024ba0908380a
Glitch-guy0 <prajwalm9845@gmail.com>
2025-04-08 20:41:50 +0530

    test: auth callback uri

--- src/auth_service/tests/test_api/test_auth.py
+++ src/auth_service/tests/test_api/test_auth.py
@@ -1,9 +1,13 @@
 import pytest
 from httpx import AsyncClient, ASGITransport
 from auth_service.api.auth import router as auth_route
-from auth_service.config import config
-
 from fastapi import FastAPI
+
+
+config = {
+    "client_id": "test_client_id",
+    "okta_authorize_redirect_uri": "http://test/oauth/v2/callback",
+}
 
 app = FastAPI()
 app.include_router(auth_route)
@@ -26,3 +30,12 @@ async def test_login_route():
         response = await ac.get("/login")
         assert response.status_code == 302
         assert response.headers["Location"] == config.okta_authorize_redirect_uri
+
+
+@pytest.mark.asyncio
+async def test_auth_callback_route():
+    async with AsyncClient(
+        transport=ASGITransport(app=app), base_url="http://test"
+    ) as ac:
+        response = await ac.get("/oauth/v2/callback", params={"code": "test_code", "state": "something"})
+        assert response.status_code == 200
