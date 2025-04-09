641c438d0d2aade6146a1a2b58e6b1ffbac3a55e
Glitch-guy0 <prajwalm9845@gmail.com>
2025-04-10 00:27:29 +0530

    refactor: indentation and file naming convention

--- src/auth_service/repository/state_token.py
+++ src/auth_service/repository/state_token.py
@@ -2,16 +2,16 @@ from dataclasses import dataclass
 from secrets import token_urlsafe
 from auth_service.models.state_token import StateToken
 
+
 @dataclass
 class StateTokenRepository:
-  def __init__(self, user_ip: str):
-    self.user_ip = user_ip
-    self.token = token_urlsafe(24)
+    def __init__(self, user_ip: str):
+        self.user_ip = user_ip
+        self.token = token_urlsafe(24)
 
-  async def get_state_token(self):
-    try:
-      await StateToken.get_state_token(self.user_ip, self.token)
-      return self.token
-    except Exception as e:
-      raise e
-
+    async def get_state_token(self):
+        try:
+            await StateToken.get_state_token(self.user_ip, self.token)
+            return self.token
+        except Exception as e:
+            raise e
--- src/auth_service/tests/models_tests/test_state_token.py
+++ src/auth_service/tests/models_tests/test_state_token.py
@@ -1,11 +1,14 @@
 import pytest
 from auth_service.repository.state_token import StateTokenRepository
 
+
 @pytest.mark.asyncio
 async def test_get_state_token(mocker):
     user_ip = "127.0.0.1"
     ###
-    db_call = mocker.patch("auth_service.models.state_token.StateToken.get_state_token", return_value=None)
+    db_call = mocker.patch(
+        "auth_service.models.state_token.StateToken.get_state_token", return_value=None
+    )
     token = await StateTokenRepository(user_ip).get_state_token()
     ###
     assert token is not None
