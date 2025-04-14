096ac7c32e66f01d0c932ba0cea58a03e0c3ecd5
Glitch-guy0 <prajwalm9845@gmail.com>
2025-04-14 20:57:20 +0530

    feat: auth service starup

---
+++ src/auth_service/main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+from auth_service.api.auth import router as auth_router
+from auth_service.lib.init_db import init_db
+from auth_service.lib.logger import logger
+
+async def lifespan(app: FastAPI):
+    logger.info("Initializing database ...")
+    print("Initializing database ...")
+    await init_db()
+    logger.info("Database initialized")
+    print("Database initialized")
+    yield
+
+app = FastAPI(lifespan=lifespan)
+
+app.include_router(auth_router)
+
+
+# test endpoint
+@app.get("/test")
+async def test():
+    logger.debug("Test endpoint called")
+    return {"message": "Hello, World!"}
