4cabe88fc738e899b75f4c2c30c7ce54be42ff1a
Glitch-guy0 <prajwalm9845@gmail.com>
2025-04-25 15:10:04 +0530

    feat(auth): Initialize MongoDB connection on startup
    - Implemented an `initialize_server` lifespan function using `asynccontextmanager`.
    - Connects to MongoDB using `DBInitializer.add_monogodb` with configuration from `Config` and registers models from `auth_service.models`.
    - Configured the FastAPI app to use this lifespan for database setup during startup and potential cleanup on shutdown.
    - Added a basic `/test` endpoint.

--- src/auth_service/main.py
+++ src/auth_service/main.py
@@ -1,22 +1,32 @@
+import sys
 from fastapi import FastAPI
-from auth_service.api.okta_api import router as auth_router
-from auth_service.lib.logger import logger
+
+# from auth_service.api.okta_api import router as auth_router
 from auth_service.config import Config
 from shared_lib.database import DBInitializer
 from auth_service.models import models
 
 
 async def initialize_server(app: FastAPI):
-    await DBInitializer.add_monogodb(Config.mongo_uri, Config.mongo_port, Config.mongo_db_name, models)
-    
+    try:
+        await DBInitializer.add_monogodb(
+            Config.mongo_uri, Config.mongo_port, Config.mongo_db_name, models
+        )
+        yield
+    except Exception:
+        print(
+            "Failed to initialize server. Please check the logs for details.",
+            file=sys.stderr,
+        )
+        sys.exit(1)
 
-app = FastAPI(lifespan=initialize_server)  # type: ignore
 
-app.include_router(auth_router)
+app = FastAPI(lifespan=initialize_server)
+
+# app.include_router(auth_router)
 
 
 # test endpoint
 @app.get("/test")
 async def test():
-    logger.debug("Test endpoint called")
     return {"message": "Hello, World!"}
