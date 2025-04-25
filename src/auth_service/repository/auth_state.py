6631ca89fb58abce220ae3c706c9a41ecdcb84ec
Glitch-guy0 <prajwalm9845@gmail.com>
2025-04-25 15:21:02 +0530

    feat: authentication userstate repository

--- src/auth_service/repository/auth_state.py
+++ src/auth_service/repository/auth_state.py
@@ -11,14 +11,13 @@ class Auth_State_Repository:
     async def save_auth_state(auth_state: AuthenticationState) -> None:
         start_time = time.monotonic()
         await AuthState.save_state(auth_state)
-        if (time.monotonic() - start_time) > 1: # if operation took more than 1sec
+        if (time.monotonic() - start_time) > 1:  # if operation took more than 1sec
             logger.warning("Auth state save operation took more than 1 second")
-                    
 
     @staticmethod
     async def get_auth_state(session_key: str) -> AuthenticationState:
         start_time = time.monotonic()
         auth_state = await AuthState.get_state(session_key)
-        if (time.monotonic() - start_time) > 1: # if operation took more than 1sec
+        if (time.monotonic() - start_time) > 1:  # if operation took more than 1sec
             logger.warning("Auth state get operation took more than 1 second")
         return auth_state
