eb8fdb493b76e09aeca2012c603041828c6c6f18
Glitch-guy0 <prajwalm9845@gmail.com>
2025-04-26 10:51:54 +0530

    wip: remove all service logs for debugging purpose

--- src/auth_service/__init__.py
+++ src/auth_service/__init__.py
@@ -1,9 +1,9 @@
-from shared_lib.logger import JSONLogger
+# from shared_lib.logger import JSONLogger
 from auth_service.config import Config
 
-ServiceLog = JSONLogger(
-    service_name=Config.service_name,
-    log_file_path=Config.log_file_path,
-    log_max_bytes=Config.log_max_bytes,
-    log_backup_count=Config.log_backup_count,
-)
+# ServiceLog = JSONLogger(
+#     service_name=Config.service_name,
+#     log_file_path=Config.log_file_path,
+#     log_max_bytes=Config.log_max_bytes,
+#     log_backup_count=Config.log_backup_count,
+# )
--- src/auth_service/lib/oidc/client/okta.py
+++ src/auth_service/lib/oidc/client/okta.py
@@ -8,7 +8,6 @@ from auth_service.config import Config
 from shared_lib.exception import ApplicationError
 from auth_service.repository.auth_state import AuthStateRepository, AuthenticationState
 from auth_service.lib.oidc.interface import Auth_Tokens
-from auth_service import ServiceLog
 
 
 class Okta_Client(OIDC_Client):
@@ -24,21 +23,11 @@ class Okta_Client(OIDC_Client):
             jwks_uri=Config.okta_jwks_uri,
             logout_uri=Config.okta_logout_uri,
         )
-        ServiceLog.debug("Okta client initialized")
 
     async def authenticaton_redirect(self) -> RedirectResponse:
         state_token = state_token_generator()
         session_key = session_key_generator()
         redirect_uri = self.application_redirect_uri
-
-        ServiceLog.debug(
-            "Generated auth parameters",
-            context={
-                "session_key": session_key,
-                "state_token": state_token,
-                "redirect_uri": redirect_uri,
-            },
-        )
 
         auth_state = AuthenticationState(
             session_key=session_key,
@@ -46,14 +35,6 @@ class Okta_Client(OIDC_Client):
             redirect_uri=redirect_uri,
         )
         await AuthStateRepository.save_auth_state(auth_state)
-        ServiceLog.info(
-            "Authentication redirect initiated",
-            context={
-                "session_key": session_key,
-                "state_token": state_token,
-                "redirect_uri": redirect_uri,
-            },
-        )
 
         params = {
             "client_id": self.client_id,
@@ -65,15 +46,6 @@ class Okta_Client(OIDC_Client):
 
         redirect_url = URL(self.authorize_uri)
         redirect_uri = redirect_url.copy_merge_params(params)
-        ServiceLog.debug(
-            "Redirect URL generated",
-            context={
-                "redirect_url": str(redirect_uri),
-                "session_key": session_key,
-                "state_token": state_token,
-                "redirect_uri": redirect_uri,
-            },
-        )
         response = RedirectResponse(
             url=str(redirect_uri),
             status_code=302,
@@ -89,26 +61,11 @@ class Okta_Client(OIDC_Client):
     async def authenticaton_callback_handler(
         self, code: str, state: str, session_key: str
     ) -> Auth_Tokens:
-        ServiceLog.debug(
-            "Processing authentication callback",
-            context={
-                "session_key": session_key,
-                "state": state,
-            },
-        )
 
         auth_state: AuthenticationState = await AuthStateRepository.get_auth_state(
             session_key
         )
         if auth_state.state_token != state:
-            ServiceLog.warning(
-                "Invalid state token received",
-                context={
-                    "session_key": session_key,
-                    "received_state": state,
-                    "expected_state": auth_state.state_token,
-                },
-            )
             raise ApplicationError("Invalid state token", 400)
 
         params = {
@@ -119,44 +76,22 @@ class Okta_Client(OIDC_Client):
             "redirect_uri": auth_state.redirect_uri,
         }
 
-        ServiceLog.debug(
-            "Attempting to exchange code for tokens",
-            context={
-                "session_key": session_key,
-                "token_uri": self.token_uri,
-            },
-        )
-
         async with AsyncClient() as client:
             response = await client.post(self.token_uri, data=params)
             try:
                 response.raise_for_status()
             except HTTPStatusError:
-                ServiceLog.error(
-                    "Failed to exchange code for tokens",
-                    context={
-                        "session_key": session_key,
-                        "error_response": response.text,
-                    },
-                )
                 raise ApplicationError(
                     f"Failed to exchange code for tokens {response.text}", 500
                 )
 
             tokens = Auth_Tokens(**response.json())
-            ServiceLog.info(
-                "Successfully obtained auth tokens",
-                context={"session_key": session_key},
-            )
             return tokens
 
     async def logout(self) -> RedirectResponse:
-        ServiceLog.debug("Logout initiated")
-        ServiceLog.info("Logout initiated")
         return RedirectResponse(url=self.logout_uri)
 
     async def get_userclaims(self, access_token: str) -> dict:
-        ServiceLog.debug("Fetching user claims")
         async with AsyncClient() as client:
             response = await client.get(
                 self.userinfo_uri, headers={"Authorization": f"Bearer {access_token}"}
@@ -164,10 +99,5 @@ class Okta_Client(OIDC_Client):
             try:
                 response.raise_for_status()
             except HTTPStatusError as e:
-                ServiceLog.error(
-                    "Failed to get user claims",
-                    context={"error": str(e)},
-                )
                 raise ApplicationError(f"Unauthorized to access userinfo", 401)
-            ServiceLog.info("Successfully retrieved user claims")
             return response.json()
