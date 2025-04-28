0ee5977dab7d932660d09ce164dca173e21caeb8
Glitch-guy0 <prajwalm9845@gmail.com>
2025-04-28 14:09:00 +0530

    feat: production environment setup

--- production/auth_service/start_service.py
+++ production/auth_service/start_service.py
@@ -1,5 +1,4 @@
 import uvicorn
-from auth_service.main import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run("auth_service.main:app", host="0.0.0.0", port=8000, workers=4)
