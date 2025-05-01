fef59f275a24e14855de29f21ea9e6f860267f04
Glitch-guy0 <prajwalm9845@gmail.com>
2025-05-01 11:45:17 +0530

    refactor: changed the file structure

--- test_make_build.bash
+++ test_make_build.bash
@@ -1,20 +1,16 @@
 #!/usr/bin/bash
 
-BuildDir="build"
-
-if [ $1 ]; then
-    BuildDir=$1
-fi
+BuildDir="builds/services/auth_service"
 
 if [ -d "$BuildDir" ]; then
     rm -rf $BuildDir
 fi
 
-mkdir $BuildDir
-cp production/auth_service/dockerfile $BuildDir/
-cp production/auth_service/docker-compose.yml $BuildDir/
-cp production/auth_service/start_service.py $BuildDir/
+mkdir -p $BuildDir
+cp production/services/auth_service/dockerfile $BuildDir/
+cp production/services/auth_service/docker-compose.yml $BuildDir/
+cp production/services/auth_service/start_service.py $BuildDir/
 
 cp requirements.txt $BuildDir/
-cp -r src/auth_service $BuildDir/
-cp -r src/shared_lib $BuildDir/
+cp -r src/services/auth_service $BuildDir/
+cp -r src/services/shared_lib $BuildDir/
