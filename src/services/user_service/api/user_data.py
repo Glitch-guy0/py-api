2743e661fc98132a513f23c5d14565ddb42db667
Glitch-guy0 <prajwalm9845@gmail.com>
2025-05-04 00:18:16 +0530

    fix: updated return message

--- src/services/user_service/api/user_data.py
+++ src/services/user_service/api/user_data.py
@@ -3,7 +3,5 @@ from shared_lib.exception import ApplicationError
 router = APIRouter()
 
 @router.get("/user")
-async def get_user_data(userdata = Header(None)):
-   if userdata is None:
-      raise ApplicationError("The server cannot meet the requirements of the Expect request-header field. Adjust the request and try again.", 417)
-   return userdata
+async def get_user_data():
+    return {"message": "user_service is working"}
