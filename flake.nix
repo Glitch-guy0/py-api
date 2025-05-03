2fb0020387e01132db5ea6e190bee11a20f13624
Glitch-guy0 <prajwalm9845@gmail.com>
2025-05-03 13:14:33 +0530

    dev: added flake dev shell

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "fastapi dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=release-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }@inputs:
+  flake-utils.lib.eachDefaultSystem 
+  (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.default = pkgs.mkShell {
+        packages = with pkgs; [
+          python3Full
+          vim
+        ];
+
+        shellHook = ''
+          export EDITOR=vim
+          if [ ! -d .venv ]; then
+            python3 -m venv .venv
+            pip install -r requirements.txt --no-cache-dir
+          fi
+          source .venv/bin/activate
+          echo "Running fastapi dev environment"
+        '';
+      };
+    }
+  );
+}
