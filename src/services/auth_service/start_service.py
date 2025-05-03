7e737ff5e9bb2ce017d7d081165e9aaafa3f997b
Glitch-guy0 <prajwalm9845@gmail.com>
2025-05-04 01:47:36 +0530

    wip: updated dockerfile

--- src/services/auth_service/start_service.py
+++ src/services/auth_service/start_service.py
@@ -1,4 +1,4 @@
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("auth_service.main:app", host="0.0.0.0", port=8000, workers=4)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, workers=4)
