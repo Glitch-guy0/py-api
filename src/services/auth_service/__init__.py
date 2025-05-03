982c76f9c8ce5dc285e6366acb280b7058909551
Glitch-guy0 <prajwalm9845@gmail.com>
2025-05-04 01:16:14 +0530

    wip: removed service as module and code and shifted to config

--- src/services/auth_service/__init__.py
+++
@@ -1,9 +0,0 @@
-from auth_service.config import Config
-from shared_lib.logger import JSONLogger
-
-ServiceLog = JSONLogger(
-    service_name=Config.service_name,
-    log_file_path=Config.log_file_path,
-    log_max_bytes=Config.log_max_bytes,
-    log_backup_count=Config.log_backup_count,
-)
--- src/services/auth_service/config.py
+++ src/services/auth_service/config.py
@@ -54,3 +54,14 @@ except ValidationError as e:
 except Exception as e:
     print(e)
     print("Error loading environment variables")
+
+
+# logger for auth service
+from shared_lib.logger import JSONLogger
+
+ServiceLog = JSONLogger(
+    service_name=Config.service_name,
+    log_file_path=Config.log_file_path,
+    log_max_bytes=Config.log_max_bytes,
+    log_backup_count=Config.log_backup_count,
+)
